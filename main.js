a9f1652aeb7bd28a6b8eb8798a3639a27b91b487
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-09-10 07:13:15 -0400

    eliminando elementos

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ $(function(){
     
     $("#btn-hide").click(function(){
         
-        // $("#segundo").text("Hola <strong>Mundo</strong>");
-        // $("#segundo").html("Hola <strong>Mundo</strong>");
-        // $("#segundo").append("Hola <strong>Mundo</strong>");
-        // $("#segundo").prepend("Hola <strong>Mundo</strong>");
-        // $("#segundo").after("<p>Hola <strong>Mundo</strong></p>");
-        $("#segundo").before("<p>Hola <strong>Mundo</strong></p>");
+        // $("#segundo").remove();
+        $("#btn-show").empty();
+
+    });
+
+    $("#btn-show").click(function(){
+        
+        $("#segundo").show();
 
     });
 
