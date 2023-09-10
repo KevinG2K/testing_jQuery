b661dc05a225f5014a99bec8c3264c2768a6d49b
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-09-10 06:41:50 -0400

    sintaxis para asegurarnos de cargar todo el documento

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-// $("h1").hide();
-$("p").hide();
+//Con esto nos aseguramos de que el documento ha terminado de cargar
+
+//1ra foma
+// $(document).ready(function(){});
+
+//2da forma
+$(function(){
+
+});
