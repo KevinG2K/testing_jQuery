89ce8e1b43acc6763653c6000197ec17bef82801
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-09-10 07:09:53 -0400

    añadiendo html dentro del parrafo

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ $(function(){
     
     $("#btn-hide").click(function(){
         
-        // alert($("#segundo").text());
-        // alert($("#segundo").html());
-        alert($("#segundo").attr("title"));
+        // $("#segundo").text("Hola <strong>Mundo</strong>");
+        // $("#segundo").html("Hola <strong>Mundo</strong>");
+        // $("#segundo").append("Hola <strong>Mundo</strong>");
+        // $("#segundo").prepend("Hola <strong>Mundo</strong>");
+        // $("#segundo").after("<p>Hola <strong>Mundo</strong></p>");
+        $("#segundo").before("<p>Hola <strong>Mundo</strong></p>");
 
     });
 
