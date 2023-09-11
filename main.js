27d9c63f85571c080bde00ab818dcc90bbf7f8a2
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-09-11 10:21:07 -0400

    atravesando el DOM

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 $(function () {
+    //CSS selector
+    //var result = $('#humanos .hombre');
 
-    $("#lista > li").addClass("red");
+    //Dom traversing
+    // var result = $('#humanos').find('.hombre');
 
-});
+    //CSS selector
+    //var result = $('#humanos > div');
+
+    //Dom traversing
+    // var result = $('#humanos').children('div');
+    // console.log(result);
+
+    //first(), last()
+    // $('#humanos').children().first().addClass('red');
+    // $('#humanos').children().last().addClass('yellow');
+
+    //prev(), next()
+    // $('#humanos').children().first().children()
+    // .last().prev().prev().addClass('red');
+    // $('#humanos').children().last().children()
+    // .first().next().next().addClass('yellow');
+
+    //para buscar elementos padres: parent(), parents()
+    $('#elegido').parents().addClass('red')
+    $('#elegida').parent().addClass('yellow')
+}); 
