3e18f5b8633f520e6fa815495896d95b7bf3ab55
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-09-18 15:31:34 -0400

    manipulando el DOM con prepend() y append()

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 $(function () {
 
-    $('#contenedor').on('click', 'a', function(e) {
-        // metodo de jQuery para anular el comportamiento predeterminado
-        e.preventDefault();
-        // metodo de jQuery para impedir la propagacion de eventos
-        e.stopPropagation();
-        // console.log('esta redireccionando');
-        console.log('ahora no se redirecciona');
+    $('#append').on('click', function(){
+        var value = $('#contenedor-agregar input').val();
+        var html = '<div class="item"><li>'+value+'</li></div>'
+        $('#contenedor-nombres').append(html);
     });
 
-    // este segundo evento es usado como ejemplo para el metodo stopPropagation()
-    $('#contenedor').on('click', function(e) {
-        console.log('click en el contenedor')
+    $('#prepend').on('click', function(){
+        var value = $('#contenedor-agregar input').val();
+        var html = '<div class="item"><li>'+value+'</li></div>'
+        $('#contenedor-nombres').prepend(html);
     });
+
 }); 
