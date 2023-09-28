2680e73136d8426ed68d3924482f1d3fd7431c1e
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-09-27 23:31:27 -0400

    manipulando el DOM con remove()

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 $(function () {
 
-    $('#after').on('click', function(){
-        var value = $('#contenedor-agregar input').val();
-        var html = '<div class="item"><li>'+value+'</li></div>'
-        $('#contenedor-nombres').children().last().after(html);
+    $('#agregar').on('click', 'button', function(){
+        var value = $('#agregar input').val();
+        var html = '<div class="item"><li>'+value+'<button class="eliminar">Eliminar</button></li></div>'
+        $('#contenedor-nombres').append(html);
     });
 
-    $('#before').on('click', function(){
-        var value = $('#contenedor-agregar input').val();
-        var html = '<div class="item"><li>'+value+'</li></div>'
-        $('#contenedor-nombres').children().first().before(html);
+    // NO SIRVE
+    // $('#contenedor-nombres .eliminar').on('click', function() {
+    //     var parent = $(this).parent().remove();
+    // });
+
+    // AHORA USANDO DELEGACION DE EVENTOS
+    $('#contenedor-nombres').on('click', '.eliminar', function() {
+        $(this).parent().remove();
     });
 
 }); 
