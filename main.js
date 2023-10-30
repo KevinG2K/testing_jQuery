f17d5d116e27d73fc9ad1b959770ad61062eb90b
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-10-29 21:03:54 -0400

    manipulando el DOM con text(), html, empty y attr.

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 $(function () {
 
-    $('#agregar').on('click', 'button', function(){
-        var value = $('#agregar input').val();
-        var html = '<div class="item"><li>'+value+'<button class="eliminar">Eliminar</button></li></div>'
-        $('#contenedor-nombres').append(html);
-    });
-
-    // NO SIRVE
-    // $('#contenedor-nombres .eliminar').on('click', function() {
-    //     var parent = $(this).parent().remove();
-    // });
-
-    // AHORA USANDO DELEGACION DE EVENTOS
-    $('#contenedor-nombres').on('click', '.eliminar', function() {
-        $(this).parent().remove();
-    });
-
+    // $('#contenedor').text();                             //para leer
+    // $('#contenedor').text("Hola Universo");              //para escribir o sobreescribir sin etiquetas.
+    // $('#contenedor').html('<strong>"Hola"</strong>');    //para escribir con etiquetas html.
+    // $('#contenedor').empty();                            //para limpiar todo
+    $('#contenedor').attr('nuevo-atributo',"xd");           //para agregar nuevo atributo
+    
 }); 
