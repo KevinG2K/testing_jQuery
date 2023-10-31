c4ab0ff1ffd9a07c0b9bb1aafd37625371e1750b
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-10-31 00:34:54 -0400

    animaciones - mostrar/ocultar con toggle()

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 $(function () {
 
-    // $('#contenedor').text();                             //para leer
-    // $('#contenedor').text("Hola Universo");              //para escribir o sobreescribir sin etiquetas.
-    // $('#contenedor').html('<strong>"Hola"</strong>');    //para escribir con etiquetas html.
-    // $('#contenedor').empty();                            //para limpiar todo
-    $('#contenedor').attr('nuevo-atributo',"xd");           //para agregar nuevo atributo
+    // delegacion de evento
+    $('.contenedor').on('click', 'a.div-href', function(e){
+        e.preventDefault();
+        // atravesando el DOM
+        // $(this).closest('.contenedor').find('.mas-info').show(1000);    //busca arriba el padre "contenedor", y luego de ahi busca el hijo "mas-info".
+                                                                        //puede tener un segundo parametro como una funcion para mostrar un alert
+                                                                        //despues que termina de ejecutarse los milisegundos definidos.
+        $(this).closest('.contenedor').find('.mas-info').toggle(1000);    //Esto muestra y oculta alternadamente.                                                                        
+    });
     
 }); 
