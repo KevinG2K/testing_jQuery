2e9951c763fd40dd97c777732ded7b8a721ec4c8
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-11-08 10:44:15 -0400

    animaciones - mostrar/ocultar con slideToggle()

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ $(function () {
     $('.contenedor').on('click', 'a.div-href', function(e){
         e.preventDefault();
         // atravesando el DOM
-        // $(this).closest('.contenedor').find('.mas-info').fadeIn(1000);  //Este efecto hace que el elemento se muestre de a poco.
-        // $(this).closest('.contenedor').find('.mas-info').fadeOut(1000);  //Este efecto hace que el elemento se oculte de a poco.
-        $(this).closest('.contenedor').find('.mas-info').fadeToggle(1000);  //Este efecto hace las dos cosas.
+        // $(this).closest('.contenedor').find('.mas-info').slideDown(1000);  //Este efecto hace que el elemento se deslice hacia abajo y desaparezca
+        // $(this).closest('.contenedor').find('.mas-info').slideUp(1000);  //Este efecto hace que el elemento se deslice hacia arriba y aparezca
+        $(this).closest('.contenedor').find('.mas-info').slideToggle(1000);  //Este efecto hace las dos cosas.
     });
     
 }); 
