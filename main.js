cc2ed950daeb542250b3a9ceef270ef38f24adc4
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-11-08 11:10:25 -0400

    animaciones usando propiedades CSS

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-$(function () {
+/*
+// SINTAXIS
+$(this). //nos referimos al elemento
+animate(    //este es el metodo
+// como 1era parametro recibe las propiedades CSS
+// como 2do parametro recibe el tiempo, puede ser unidades o palabras (slow, fast)
+// como 3er parametro se puede ejecutar una funcion.
+{}, '', function(){}
+);
+*/
 
-    // delegacion de evento
-    $('.contenedor').on('click', 'a.div-href', function(e){
-        e.preventDefault();
-        // atravesando el DOM
-        // $(this).closest('.contenedor').find('.mas-info').slideDown(1000);  //Este efecto hace que el elemento se deslice hacia abajo y desaparezca
-        // $(this).closest('.contenedor').find('.mas-info').slideUp(1000);  //Este efecto hace que el elemento se deslice hacia arriba y aparezca
-        $(this).closest('.contenedor').find('.mas-info').slideToggle(1000);  //Este efecto hace las dos cosas.
+$(function () {
+    $('#cuadrado1').on('click', function() {
+        $(this).animate({
+            opacity: 0.3
+        }, 'fast', function() {
+            alert('cuadrado1');
+        });
     });
-    
-}); 
+
+    $('#cuadrado2').on('click', function() {
+        $(this).animate({
+            left: 200
+        }, 'slow');
+    });
+
+    $('#cuadrado3').on('click', function() {
+        $(this).animate({
+            left: '+=50', width: 200
+        }, 500);
+    });
+});
